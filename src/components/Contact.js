426054ab8e7b421d625a080f8612542316c42b8c
MuizU <muizuvais99@gmail.com>
2021-08-02 12:11:02 +0530

    switched to react

---
+++ src/components/Contact.js
@@ -0,0 +1,106 @@
+import React, { Component, useState } from "react";
+export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const encode = (data) =>
+    Object.keys(data).map(
+      (key) =>
+        encodeURIComponent(key) + "=" + encodeURIComponent(data[key]).join("&")
+    );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-wwww-form-url-encoded" },
+        body: encode({ "form-name": "contact", name, email, message }),
+      });
+    } catch (e) {
+      alert(e);
+    }
+  };
+  return (
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            src="https://www.google.com/maps/embed/v1/place?q=Dehiwala-Mount+Lavinia&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-wodest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                11/14 St Marys Road <br />
+                Mount Lavinia 10370, Western Province, Sri Lanka
+              </p>
+            </div>
+            <div className="px-6 mt-4 lg:mt-0 lg:w-1/2">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                muizuvais99@gmail.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">&plus;94721160797</p>
+            </div>
+          </div>
+        </div>
+        <form
+          action=""
+          netlifyname="contact"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Hire Me
+          </h2>
+          <p className="leading-relaxed mb-5">
+            Get in touch with me to build your next project
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="w-full bg-gray-800 rounded border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="realtive mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            ></textarea>
+          </div>
+          <div className="button:submit text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Submit
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Crypto-Watch",
+    subtitle: "React.js, Bootstrap, Chart.js CoinCap API ",
+    description:
+      "A cryptocurrency tracker which displays the cryptocurrency prices and price history, as well as fiat currency prices, and details about crypto exchanges. This web app uses the CoinCap API.",
+    image: "./project-1.gif",
+    link: "https://crypto-watch.tk",
+  },
+  {
+    title: "Lyric-Findr",
+    subtitle: "React.js, Bootstrap, Shazam API",
+    description:
+      "This app enables the user to search for lyrics of songs. It is powered by the Shazam API",
+    image: "./project-2.gif",
+    link: "https://lyric-findr.tk/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "Python",
+  "React",
+  "Nuxt.js",
+  "Flask",
+  "Bootstrap",
+];
