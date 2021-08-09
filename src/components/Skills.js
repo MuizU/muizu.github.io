59d151200d7740d5c9bd58e5f54a1d0a09d29282
MuizU <muizuvais99@gmail.com>
2021-08-09 15:53:25 +0530

    renamed the skills file

---
+++ src/components/Skills.js
@@ -0,0 +1,34 @@
+import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
+import React, { Component } from "react";
+import { skills } from "../data";
+export default class Skills extends Component {
+  render() {
+    return (
+      <section id="skills">
+        <div className="text-center mb-20">
+          <ChipIcon className="w-10 inline-block mb-4"></ChipIcon>
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+            Skills &amp; Technologies
+          </h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+            After my four years of Higher Education combined with my work
+            experience I have gained a lot of skills and learned new
+            technologies along the way.
+          </p>
+        </div>
+        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+          {skills.map((skill) => (
+            <div key="skill" className="p-2 w-full sm:w-1/2">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="title-font font-medium text-white">
+                  {skill}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
