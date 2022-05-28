70715556b361aadda1adda177b7bdf4452ffbc07
MuizU <>
2022-05-28 14:11:14 +0000

    Refactored the Contact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,109 +1,33 @@
-import React, { Component, useState } from "react";
-import { useForm, ValidationError } from "@formspree/react";
+import React from "react";
 
 export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [state, handleSubmit] = useForm("mbjqzkkk");
 
-  if (state.succeeded) {
-    return (
-      <p>
-        Thanks for Contacting!
-        <br />
-        Will get back to you shortly
-      </p>
-    );
-  }
-
-  function* afterFormSubmitSuccess() {}
   return (
     <section id="contact" className="relative">
+      <header className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap justify-center">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+          Contact Me</h1>
+
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          If you've seen my potential or want to talk to me, don't hesitate to send me a message.</p>
+      </header>
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
-            src="https://www.google.com/maps/embed/v1/place?q=Dehiwala-Mount+Lavinia&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-            width="100%"
-            height="100%"
-            title="map"
-            className="absolute inset-0"
-            frameBorder={0}
-            marginHeight={0}
-            marginWidth={0}
-            style={{ filter: "opacity(0.7)" }}
-          />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-wodest text-xs">
-                ADDRESS
-              </h2>
-              <p className="mt-1">
-                Mount Lavinia 10370, Western Province, Sri Lanka
-              </p>
-            </div>
-            <div className="px-6 mt-4 lg:mt-0 lg:w-1/2">
+            <div className="px-6 lg:w-1/2">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a href={'mailto:muizuvais99@gmail.com'} 
+                className="no-underline hover:underline">
                 muizuvais99@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">&#43;94 721 160 797</p>
+              <a href={"tel:+94765357344"}
+                className="no-underline hover:underline"
+
+              >&#43;94 765 357 344</a>
             </div>
-          </div>
-        </div>
-        <form
-          action=""
-          novalidate
-          onSubmit={handleSubmit}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-        >
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
-          </h2>
-          <p className="leading-relaxed mb-5">
-            Get in touch with me to build your next project or to join your
-            team.
-          </p>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-              Email
-            </label>
-            <input
-              type="text"
-              required
-              name="email"
-              id="email"
-              className="w-full bg-gray-800 rounded border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="realtive mb-4">
-            <label
-              htmlFor="message"
-              className="leading-7 text-sm text-gray-400"
-            >
-              Message
-            </label>
-            <textarea
-              required
-              id="message"
-              name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-            ></textarea>
-          </div>
-          {}
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-            disabled={state.submitting}
-          >
-            Submit
-          </button>
-        </form>
       </div>
     </section>
   );
