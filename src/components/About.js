85dd8a9e316cbda6b0cff6e1abe69fb92a60b6f5
MuizU <muizuvais99@gmail.com>
2022-05-29 14:06:53 +0530

    Refactored grammatical error

--- src/components/About.js
+++ src/components/About.js
@@ -19,7 +19,7 @@ export default class About extends Component {
                 href="#contact"
                 className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
               >
-                Hire With Me
+                Hire Me
               </a>
               <a
                 href="#projects"
